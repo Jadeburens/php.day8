f02acb81459565b845807d21fcd802b668ddd5a5
jade.burens <jade.burens@gmail.com>
2021-07-08 15:49:10 +0200

    yeeepa

---
+++ ex_03/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+    if (isset($_GET["name"]))
+        $_SESSION["name"] = htmspecialchars($_GET["name"]);
+    if (isset($_SESSION["name"]))
+        echo "Hello" . $_SESSION["name"];
+    else 
+        echo "Hello patypus";
+?>
